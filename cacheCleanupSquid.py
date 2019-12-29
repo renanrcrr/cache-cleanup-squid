e7d59b87264bd5e0601d4c652bcec02f7e321aec
Renan Rodrigues <renanrcrr.developer@gmail.com>
2019-12-30 00:29:57 +1100

    resolving some mistakes with spaces

--- cacheCleanupSquid.py
+++ cacheCleanupSquid.py
@@ -7,21 +7,21 @@ class CacheSquid:
 	def limparCache(self):
 		os.system("/etc/init.d/squid3 stop")
         
-        sleep(15)
+        	sleep(15)
         
-        os.system("mv /var/spool/squid3 /var/spool/squid3.del")
+        	os.system("mv /var/spool/squid3 /var/spool/squid3.del")
         
-        os.system("mkdir /var/spool/squid3")
+        	os.system("mkdir /var/spool/squid3")
         
-        os.system("chmod 777 /var/spool/squid3")
+        	os.system("chmod 777 /var/spool/squid3")
         
-        os.system("/var/spool/squid3 -z")
+        	os.system("/var/spool/squid3 -z")
         
-        sleep(3000)
+        	sleep(3000)
         
-        os.system("/etc/init.d/squid3 start")
+        	os.system("/etc/init.d/squid3 start")
         
-        os.system("rm -rf /var/spool/squid3.del")
+        	os.system("rm -rf /var/spool/squid3.del")
 
 	def main():
 		cs = CacheSquid()
